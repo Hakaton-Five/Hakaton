53226b5cd4abec542428e2a0aa4e23e728ed9f6a
Bogdan Konotop <bogdankonotop@Bogdans-MacBook-Pro.local>
2025-01-11 16:31:17 +0200

    refactor sidebar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {FolderComponent} from "./pages/folder/folder.component";
 import {ChatComponent} from "./pages/chat/chat.component";
 import {ProjectsComponent} from "./pages/projects/projects.component";
 import { ROUTES } from "./core/constants/routes";
+import { PlanningComponent } from './pages/planning/planning.component';
 
 
 export const routes: Routes = [
@@ -15,6 +16,7 @@ export const routes: Routes = [
   { path: ROUTES.Folder, component: FolderComponent },
   { path: ROUTES.Chat, component: ChatComponent },
   { path: ROUTES.Project, component: ProjectsComponent },
+  { path: ROUTES.Planning, component: PlanningComponent },
   { path: '', redirectTo: `/${ROUTES.Dashboard}`, pathMatch: 'full' },
   { path: '**', redirectTo: `/${ROUTES.Dashboard}` }
 ];
