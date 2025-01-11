7b0c59d983ec8328bfa7a1da950c3392821dcf69
Maksym <komisat155@gmail.com>
2025-01-11 15:33:43 +0200

    add sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {SidebarComponent} from "./shared/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CheckboxModule, FormsModule, CommonModule],
+  imports: [RouterOutlet, CheckboxModule, FormsModule, CommonModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Hakaton';
-  public checked = true;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { appConfig } from './app/app.config'; // Import your appConfig
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig, // Spread appConfig to include its settings
+  providers: [
+    ...appConfig.providers, // Spread any existing providers from appConfig
+    importProvidersFrom(BrowserAnimationsModule) // Add BrowserAnimationsModule
+  ]
+}).catch(err => console.error(err));
