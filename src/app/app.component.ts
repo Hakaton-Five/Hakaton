cee003ccfdfe8f246949baf426c02c5ad32a96bd
July <85956057+Juliet-cyborg@users.noreply.github.com>
2025-01-11 15:18:27 +0200

    implement routes (#8)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { InputTextComponent } from './shared/components/input-text/input-text.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MemberCardComponent } from './shared/components/member-card/member-card.component';
 import { USERS } from './core/mock/users';
