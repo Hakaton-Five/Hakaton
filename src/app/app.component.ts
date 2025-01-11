14dd39e22b8263aa01145a0e5fc67736e2613527
Hover19 <klaskintrade@gmail.com>
2025-01-11 15:06:57 +0200

    add member-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { InputTextComponent } from './shared/components/input-text/input-text.component';
-import {HeaderComponent} from "./shared/header/header.component";
+import { HeaderComponent } from './shared/header/header.component';
+import { MemberCardComponent } from './shared/components/member-card/member-card.component';
+import { USERS } from './core/mock/users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CheckboxModule, FormsModule, CommonModule, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    MemberCardComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -18,4 +25,5 @@ export class AppComponent {
   public checked = true;
 
   public inputValue = 'Qwerty';
+  public users = USERS;
 }
