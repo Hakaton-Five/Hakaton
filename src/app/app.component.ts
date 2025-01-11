a6ad11689c4226d0061db6df41a005faf4a870d9
Bogdan Konotop <bogdankonotop@Bogdans-MacBook-Pro.local>
2025-01-11 14:41:13 +0200

    Add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { InputTextComponent } from './shared/components/input-text/input-text.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CheckboxModule, FormsModule, CommonModule],
+  imports: [RouterOutlet, CheckboxModule, FormsModule, CommonModule, InputTextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Hakaton';
   public checked = true;
+
+  public inputValue = 'Qwerty';
 }
